26a7f24ee6da5c16e5ff454d7483bae77474cdaf
Anish Dutta <60481056+anishdutta@users.noreply.github.com>
2021-09-26 14:04:34 +0530

    User Details done

--- index.js
+++ index.js
@@ -1,30 +1,95 @@
-const express = require("express")
-var bodyParser = require('body-parser')
-const db = require('./config/db')
-const app = express()
-require("dotenv").config()
+const express = require("express");
+var bodyParser = require("body-parser");
+const db = require("./config/db");
+const app = express();
+require("dotenv").config();
+
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get("/api/getUserDetails/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("SELECT * FROM UserDetails WHERE UserId = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+
+    res.send(result);
+  });
+});
+
+app.post("/api/createUser", (req, res) => {
+  const UserId = req.body.UserId;
+  const EmailId = req.body.EmailId;
+  const BirthDate = req.body.BirthDate;
+  const Gender = req.body.Gender;
+  const Name = req.body.Name;
+  const ProfileImg = req.body.ProfileImg;
+  const InstaId = req.body.InstaId;
+  const Vaccine = req.body.Vaccine;
 
 
-const cors = require("cors")
-const nodemailer = require("nodemailer")
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+  db.query(
+    `insert into UserDetails (UserId,EmailId,BirthDate,Gender,Name,ProfileImg,InstaId,Vaccine ) values ('${UserId}', '${EmailId}', '${BirthDate}', '${Gender}', '${Name}',  '${ProfileImg}', '${InstaId}', '${Vaccine}');`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(result);
+    }
+  );
+  res.send("Succefully added to db");
+});
 
-app.use(cors())
+app.get("/api/getFromId/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("SELECT * FROM myusers WHERE Id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
 
-app.get("/api/getFromId/:id", (req,res)=>{
+app.post("/api/createUserPhotos", (req, res) => {
+  const Id = req.body.Id;
+  const UserId = req.body.UserId;
+  const EmailId = req.body.EmailId;
+  const PhotoUrl = req.body.PhotoUrl;
+  
 
+
+  db.query(
+    `insert into UserPhotos (Id,UserId,EmailId,PhotoUrl  ) values ('${Id}', '${UserId}', '${EmailId}', '${PhotoUrl}');`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send(err.sqlMessage);
+      }
+      else{
+          console.log(result);
+          res.send("Succefully added to db");
+      }
+      
+    }
+  );
+  
+});
+
+app.get("/api/getUserPhotos/:id", (req, res) => {
     const id = req.params.id;
-        db.query("SELECT * FROM user WHERE Id = ?", id, (err,result)=>{
-            if(err) {
-            console.log(err)
-            } 
-            
-        res.send(result)
-        }
-        );   
-        });
+    db.query("SELECT * FROM UserPhotos WHERE UserId = ?", id, (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(result);
+    });
+  });
 
 app.listen(process.env.PORT || 4000, () => {
-	console.log(`Example app listening at http://localhost: 4000`)
-  })
+  console.log(`Example app listening at http://localhost: 4000`);
+});
